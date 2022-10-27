25f717b1b0b3f85011dcf4372e3674db489cfb5b
Noxi1 <116846961+Noxi1@users.noreply.github.com>
2022-10-28 00:04:22 +0200

    Add files via upload

---
+++ projket i ri/book_to_read.js
@@ -0,0 +1,12 @@
+var page1 = document.getElementById('page_1')
+var review = document.getElementById('review')
+
+page1.addEventListener(('click'),function() {
+    var mainImg = document.getElementById('main_img').src = './book1.jpg'
+    var img2 = document.getElementById('img2').src = './from-blood-to-ash.jpg'
+    var img3 = document.getElementById('img3').src = './it_starts_with_us.jpg'
+    var img4 = document.getElementById('img4').src = './it_starts_with_us.jpg'
+    var img5 = document.getElementById('img5').src = './it_starts_with_us.jpg'
+    var img6 = document.getElementById('img6').src = './it_starts_with_us.jpg'
+    var img7 = document.getElementById('img7').src = './it_starts_with_us.jpg'
+})
